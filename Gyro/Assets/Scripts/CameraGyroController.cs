7f31c74ac816c7d30f70ff66eb88b0f722bf6e5c
Gabriel_Zanini <gabriel.roszan@gmail.com>
2020-08-27 12:54:30 -0300

    Gyro working.

---
+++ Gyro/Assets/Scripts/CameraGyroController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGyroController : MonoBehaviour
+{
+    public Quaternion defaultRot = Quaternion.identity;
+    public Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
+
+
+
+    void Start()
+    {
+        GyroManager.Instance.EnableGyro();
+    }
+
+
+    void Update()
+    {
+        transform.localRotation = GyroManager.Instance.GetGyroRotation() * baseRotation;
+    }
+
+    
+    public void ResetRotation()
+    {
+        defaultRot = transform.localRotation;
+    }
+}
